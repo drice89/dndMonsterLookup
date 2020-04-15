af4b87dcb0161ed0667e9d96e29430211ffc4997
drice89 <drice89@gmail.com>
2020-04-15 00:02:46 -0700

    Initial Commit

---
+++ frontend/root.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import MonsterList from './monster_list'
+
+class Root extends React.Component {
+
+  render(){
+    return(
+      <div>
+        <MonsterList />
+      </div>
+
+    )
+  }
+
+}
+
+export default Root;
